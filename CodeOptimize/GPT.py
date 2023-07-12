b23448c0d95a0315fd854866aa1610cba6717db5
Xinyuan  Ye <u6296255@anu.edu.au>
2023-07-12 20:14:25 +0800

    Added exception checking functions to the GPT class

--- CodeOptimize/GPT.py
+++ CodeOptimize/GPT.py
@@ -52,7 +52,7 @@ class GPT:
         Generates a corrected version of a code snippet based on user feedback.
 
         :param code_snippet: The original code snippet.
-        :param feedback: The user's feedback on the code snippet.
+        :param feedback: The feedback on the code snippet.
         :return: The corrected code snippet.
         """
         prompt = code_snippet + "\n" + feedback
@@ -96,6 +96,74 @@ class GPT:
         prompt_string = "\n".join(prompts)
 
         return prompt_string
+
+    def check_exceptions_handled(self, code_snippet: str, fqn: str, exception_to_check: str) -> bool:
+        """
+        Check if a specific exception is handled or caught for a given fully qualified name (fqn) in a code snippet.
+
+        :param code_snippet: The code snippet to analyze for exception handling.
+        :param fqn: The fully qualified name of the API associated with the exception.
+        :param exception_to_check: The exception to check if it is handled or caught.
+        :return: True if the exception is handled or caught in the code snippet, False otherwise.
+        """
+
+        exception_check_prompt = f"Is the {exception_to_check} handled or caught for {fqn} in the code snippet? Answer in Yes or No only."
+
+        prompt = code_snippet + "\n" + exception_check_prompt
+        self.conversation.append({"role": "user", "content": prompt})
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=self.conversation,
+            temperature=0.0
+        )
+
+        answer = response["choices"][0]["message"]["content"]
+
+        self.conversation.append({"role": "assistant", "content": answer})
+
+        if answer.lower().startswith("no."):
+            return False
+
+        return True
+
+    def add_exceptions(self, code_snippet: str, exceptions_feedback_prompt: str):
+        """
+        Adds exceptions to the provided code snippet based on the feedback prompt.
+
+        :param code_snippet: The code snippet to which exceptions will be added.
+        :param exceptions_feedback_prompt: The exceptions' feedback on the code snippet.
+        :return: The corrected code snippet with added exceptions.
+        """
+
+        prompt = code_snippet + "/n" + exceptions_feedback_prompt
+        self.conversation.append({"role": "user", "content": prompt})
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=self.conversation,
+            temperature=0.0
+        )
+
+        corrected_code = response["choices"][0]["message"]["content"]
+
+        self.conversation.append({"role": "assistant", "content": corrected_code})
+
+        return corrected_code
+
+    def generate_exceptions_prompts(self, exceptions_to_add: list[tuple[str, str, str]]) -> str:
+        """
+        Generates prompts for a given list of exceptions to add.
+
+        :param exceptions_to_add: A list of triples (fqn, exception_to_add, condition) representing the exceptions to add.
+        :return: A string containing the generated prompts for each exception.
+        """
+        exception_feedback_prompt = ""
+        for fqn, exception_to_add, condition in exceptions_to_add:
+            condition = condition.replace("This exception is thrown when ", "if ")
+            exception_feedback_prompt += f"Please check {condition} for {fqn} in the given code snippet, otherwise throw {exception_to_add}.\n"
+
+        return exception_feedback_prompt
 
     def manual_add_response(self, response: str):
         self.conversation.append({"role": "assistant", "content": response})
