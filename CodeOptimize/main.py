9e160e0788160694517a314bbaf1bc6bbf032b75
Xinyuan  Ye <u6296255@anu.edu.au>
2023-07-12 20:15:07 +0800

    Implemented the exception checking process

--- CodeOptimize/main.py
+++ CodeOptimize/main.py
@@ -1,6 +1,7 @@
 import os
 import openai
 from CodeChecker import *
+from ExceptionChecker import *
 from CodeOptimize.code_fix import fix_code
 
 def parse_code(js_file):
@@ -15,6 +16,8 @@ def get_few_shots(few_shot_file):
 
 fqn_prompts_file = os.getcwd() + "prompts_templates/fqn_prompts.txt"
 code_generate_prompts_file = os.getcwd() + "prompts_templates/code_generate_prompts.txt"
+exception_check_prompts_file = os.getcwd() + "prompts_templates/exceptions_check_prompts.txt"
+exception_add_prompts_file = os.getcwd() + "prompts_templates/exceptions_add_prompts.txt"
 code_fix_prompts_file = os.getcwd() + "prompts_templates/code_fix_prompt.txt"
 
 # js_file = "../data/Random Roll Call Page/app.js"
@@ -24,6 +27,8 @@ code_fix_prompts_file = os.getcwd() + "prompts_templates/code_fix_prompt.txt"
 def main(code_file_path: str):
     fqn_prompts = get_few_shots(fqn_prompts_file)
     code_generate_prompts = get_few_shots(code_generate_prompts_file)
+    exception_check_prompts = get_few_shots(exception_check_prompts_file)
+    exception_add_prompts = get_few_shots(exception_add_prompts_file)
     code_fix_prompts = get_few_shots(code_fix_prompts_file)
     code_snippet = parse_code(js_file=code_file_path)
 
@@ -39,7 +44,14 @@ def main(code_file_path: str):
     print("Code checking...")
     codeChecker = CodeChecker(kg, fqn_prompts, code_generate_prompts)
     refined_code = codeChecker.code_check(code_snippet, 10)
-    print("Code check completed. Refined code:")
+    print("Code analysis completed. Refined code:")
+    print(refined_code)
+    print()
+
+    print("Checking exceptions in code...")
+    exceptionChecker = ExceptionChecker(kg, fqn_prompts, exception_check_prompts=exception_check_prompts, exception_handle_prompts=exception_add_prompts)
+    exceptionChecker.code_check(refined_code)
+    print(f"Exception code analysis completed. Refined code:")
     print(refined_code)
     print()
 
