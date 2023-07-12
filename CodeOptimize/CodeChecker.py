0093e5e9e9a1cfd8575116d17fa557d6d71d3537
Xinyuan  Ye <u6296255@anu.edu.au>
2023-07-13 01:21:13 +0800

    Beautified console output

--- CodeOptimize/CodeChecker.py
+++ CodeOptimize/CodeChecker.py
@@ -34,7 +34,7 @@ class CodeChecker:
             fqn_list = fqn_parser.get_apis_in_code(code_snippet)
             print("Extracting Fully Qualified Names...")
             for index, fqn in enumerate(fqn_list, start=1):
-                print(f"{index}. {fqn}")
+                print(f"    {index}. {fqn}")
             print()
 
             invalid_api_list = []
@@ -43,7 +43,7 @@ class CodeChecker:
             print(f"Checking invalid/deprecated APIs...")
             for fqn in fqn_list:
                 valid = check_valid(fqn, self.knowledge_graph)
-                if not None:
+                if not valid:
                     invalid_api_list.append(fqn)
                     continue
                 deprecated = check_deprecated(valid)
@@ -53,28 +53,53 @@ class CodeChecker:
             if not invalid_api_list and not deprecated_api_list:
                 print(f"No invalid APIs present in the code. ")
                 print(f"No deprecated APIs present in the code")
-                print(f"Loop count: {loop_count + 1}")
+                print(f"The number of iterations used to rectify the code: {loop_count + 1}")
                 print(f"Done.\n")
                 break
 
-            print(f"{len(invalid_api_list)} invalid apis found:")
-            for index, fqn in enumerate(invalid_api_list, start=1):
-                print(f"{index}. {fqn}")
-            print()
+            if loop_count + 1 == max_loop:
+                print(f"The maximum loop limit of {max_loop} has been reached!")
+                if len(invalid_api_list) > 0:
+                    print(f"There are still {len(invalid_api_list)} invalid APIs present in the code:")
+                    for fqn in invalid_api_list:
+                        print(f"    * {fqn}")
+                    print()
+                else:
+                    print("No invalid APIs present in the code.")
 
-            print(f"{len(deprecated_api_list)} invalid apis found:")
-            for index, fqn in enumerate(deprecated_api_list, start=1):
-                print(f"{index}. {fqn}")
-            print()
+                if len(deprecated_api_list) > 0:
+                    print(f"There are still {len(deprecated_api_list)} deprecated APIs present in the code:")
+                    for fqn in deprecated_api_list:
+                        print(f"    *. {fqn}")
+                    print()
+                else:
+                    print("No deprecated APIs present in the code.\n")
+
+                break
+
+            if len(invalid_api_list) > 0:
+                print(f"{len(invalid_api_list)} invalid APIs found:")
+                for fqn in invalid_api_list:
+                    print(f"    * {fqn}")
+                print()
+            else:
+                print(f"{len(invalid_api_list)} invalid APIs found.")
+
+            if len(deprecated_api_list) > 0:
+                print(f"{len(deprecated_api_list)} deprecated APIs found:")
+                for fqn in deprecated_api_list:
+                    print(f"    *. {fqn}")
+                print()
+            else:
+                print(f"{len(deprecated_api_list)} deprecated APIs found.")
+                print()
+
+            print(f"Providing feedback on invalid or deprecated APIs to rectify the code...")
 
             feedback_prompt = code_generator.generate_feedback_prompt(invalid_api_list, deprecated_api_list)
             corrected_code = code_generator.feedback4correction(code_snippet, feedback_prompt)
             code_snippet = corrected_code
             loop_count += 1
-
-            if loop_count == max_loop:
-                print(f"Max number of loop reached..")
-                print(f"There are still {len(invalid_api_list)} invalid APIs present in the code.")
-                print(f"There are still {len(deprecated_api_list)} deprecated APIs present in the code.")
+            print(f"Done! Now rechecking the code to ensure there are no invalid or deprecated APIs...\n")
 
         return code_snippet
